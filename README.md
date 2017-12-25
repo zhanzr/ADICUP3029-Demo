7c6242fdc7bc8bffa3738b41166e0ea6a4a129f1
zhanzr <zhanzr@foxmail.com>
2017-12-26 01:38:07 +0800

    dhrystone demo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ADICUP3029-Demo
 Demo for ADICUP3029 board.
+
+Dhrystone benchmark.
+
+ARMCC 5.06, all optimization enabled. With Microlib:
+uSeconds for one run through Dhrystone[29-6509]: 21.6
+Dhrystone per Second:	46296.3
+
+Without Microlib:
+uSeconds for one run through Dhrystone[29-6509]: 15.7
+Dhrystone per Second:	63829.8
+
---
+++ RTE/_dhrystone/RTE_Components.h
@@ -0,0 +1,28 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'adicup3029_t1' 
+ * Target:  'dhrystone' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ADuCM3029.h"
+
+#define RTE_Compiler_IO_STDERR          /* Compiler I/O: STDERR */
+          #define RTE_Compiler_IO_STDERR_User     /* Compiler I/O: STDERR User */
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+          #define RTE_Compiler_IO_STDIN_User      /* Compiler I/O: STDIN User */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
+#define RTE_Compiler_IO_TTY             /* Compiler I/O: TTY */
+          #define RTE_Compiler_IO_TTY_User        /* Compiler I/O: TTY User */
+
+#endif /* RTE_COMPONENTS_H */
